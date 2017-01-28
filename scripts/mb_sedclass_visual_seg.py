9b8a0b49b423f488b17d9afefa045ee6a02d87f5
danhamill <hamill.daniel@gmail.com>
2017-01-28 12:30:09 -0700

    inital commit

--- scripts/mb_sedclass_visual_seg.py
+++ scripts/mb_sedclass_visual_seg.py
@@ -119,16 +119,16 @@ if __name__ == '__main__':
 
     circ5 = Line2D([0], [0], linestyle="none", marker="o", markersize=10, markerfacecolor=colors[4],alpha=a_val)
     #Total extents (i.e. misplaced SS and multibeam)
-    fig, (ax,ax1) = plt.subplots(figsize=(12,10),ncols=2)
+    fig, (ax,ax1) = plt.subplots(figsize=(10,8),ncols=2)
     
     #Start with april sidescan sonar
     ax.set_title('April 2014 \n Sidescan Sonar Imagery')
     m = Basemap(projection='merc', 
                 epsg=cs2cs_args.split(':')[1], 
-                llcrnrlon=np.nanmin(april_lon)-0.0009, 
-                llcrnrlat=np.nanmin(april_lat)-0.0006,
-                urcrnrlon=np.nanmax(april_lon)+0.0009, 
-                urcrnrlat=np.nanmax(april_lat)+0.0006,ax=ax)
+                llcrnrlon=np.nanmin(april_lon)-0.0006, 
+                llcrnrlat=np.nanmin(april_lat)-0.0003,
+                urcrnrlon=np.nanmax(april_lon)+0.0006, 
+                urcrnrlat=np.nanmax(april_lat)+0.0003,ax=ax)
     m.wmsimage(server='http://grandcanyon.usgs.gov/arcgis/services/Imagery/ColoradoRiverImageryExplorer/MapServer/WmsServer?', layers=['3'], xpixels=1000)
     x,y = m.projtran(april_lon, april_lat)
     im = m.contourf(x,y,april_ss.T, cmap='Greys_r', levels=[0,2.5,5,7.5,10,12.5,15,17.5,20,22.5,25,27.5,30,32.5,35])
@@ -147,11 +147,116 @@ if __name__ == '__main__':
     x,y = m.projtran(may_lon, may_lat)
     im = m.contourf(x,y,may_sed_class.T, cmap='coolwarm', levels=[0,1,2,3,4,5])
    
-    ax1.legend((circ5, circ3,circ1),('Sand','Gravel','Gravel'),numpoints=1, loc='best')
+    ax1.legend((circ5, circ3,circ1),('Sand','Gravel','Rocks'),numpoints=1, loc='best')
     plt.tight_layout()
     plt.show()
     plt.savefig(root + os.sep + 'misaligned_ss_imagery_example.png',dpi=600)
     
+#########################################################################################################################################
+    #Load May 2014 Data
+   
+    ds = gdal.Open(r"C:\workspace\Reach_4a\Multibeam\mb_sed_class\input\may_14_mbsed3class.tif")
+    may_sed_class = ds.GetRasterBand(1).ReadAsArray()
+    may_sed_class[may_sed_class<0]=np.nan
+    gt = ds.GetGeoTransform()
+    proj = ds.GetProjection()
+    
+    xres = gt[1]
+    yres = gt[5]
+    
+    # get the edge coordinates and add half the resolution 
+    # to go to center coordinates
+    xmin = gt[0] + xres * 0.5
+    xmax = gt[0] + (xres * ds.RasterXSize) - xres * 0.5
+    ymin = gt[3] + (yres * ds.RasterYSize) + yres * 0.5
+    ymax = gt[3] - yres * 0.5
+    extent = [xmin,xmax,ymin,ymax]
+    del ds
+    
+    xx, yy = np.mgrid[xmin:xmax+xres:xres, ymax+yres:ymin:yres]
+    may_lon, may_lat = trans(xx, yy, inverse=True)
+    del xx, yy, xmin, xmax, ymin, ymax
+    
+    #Load April_2014 SS data
+   
+    ds = gdal.Open(ss_raster)
+    april_ss = ds.GetRasterBand(1).ReadAsArray()
+    april_ss[april_ss<0]=np.nan
+    gt = ds.GetGeoTransform()
+    proj = ds.GetProjection()
+    
+    xres = gt[1]
+    yres = gt[5]
+    
+    # get the edge coordinates and add half the resolution 
+    # to go to center coordinates
+    xmin = gt[0] + xres * 0.5
+    xmax = gt[0] + (xres * ds.RasterXSize) - xres * 0.5
+    ymin = gt[3] + (yres * ds.RasterYSize) + yres * 0.5
+    ymax = gt[3] - yres * 0.5
+    extent = [xmin,xmax,ymin,ymax]
+    del ds
+    
+    xx, yy = np.mgrid[xmin:xmax+xres:xres, ymax+yres:ymin:yres]
+    april_lon, april_lat = trans(xx, yy, inverse=True)
+    del xx, yy, xmin, xmax, ymin, ymax
 
+    colors=['#ca0020','#f4a582','#f7f7f7','#92c5de','#0571b0']
+    a_val=1
+    circ1 = Line2D([0], [0], linestyle="none", marker="o", markersize=10, markerfacecolor=colors[0],alpha=a_val)
 
+    circ3 = Line2D([0], [0], linestyle="none", marker="o", markersize=10, markerfacecolor=colors[2],alpha=a_val)
 
+    circ5 = Line2D([0], [0], linestyle="none", marker="o", markersize=10, markerfacecolor=colors[4],alpha=a_val)
+   
+    
+    #Total extents (i.e. misplaced SS and multibeam)
+    fig, (ax,ax1) = plt.subplots(figsize=(13,8),ncols=2)
+    
+    #Start with april sidescan sonar
+    ax.set_title('April 2014 \n Sidescan Sonar Imagery')
+    m = Basemap(projection='merc', 
+                epsg=cs2cs_args.split(':')[1], 
+                llcrnrlon=np.nanmin(april_lon)-0.0001, 
+                llcrnrlat=np.nanmin(april_lat)-0.0002,
+                urcrnrlon=np.nanmax(april_lon)+0.0001, 
+                urcrnrlat=np.nanmax(april_lat)+0.0002,ax=ax)
+    m.wmsimage(server='http://grandcanyon.usgs.gov/arcgis/services/Imagery/ColoradoRiverImageryExplorer/MapServer/WmsServer?', layers=['3'], xpixels=1000)
+    x,y = m.projtran(april_lon, april_lat)
+    im = m.contourf(x,y,april_ss.T, cmap='Greys_r', levels=[0,2.5,5,7.5,10,12.5,15,17.5,20,22.5,25,27.5,30,32.5,35])
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes("right", size="5%", pad=0.1)
+    cbr = plt.colorbar(im, cax=cax)
+    cbr.set_label('Sidescan Intensity [dBW]')
+    m.readshapefile(r"C:\workspace\Reach_4a\Multibeam\mb_sed_class\output\shapefiles\visual_seg_geo","layer",drawbounds = False)
+    #sand, sand/gravel, gravel, sand/rock, rock
+    s_patch,  g_patch, r_patch,  = [],[],[]
+    for info, shape in zip(m.layer_info, m.layer):
+        if info['substrate'] == 'sand' :
+            s_patch.append(Polygon(np.asarray(shape),True))
+       
+        if info['substrate'] == 'gravel':
+            g_patch.append(Polygon(np.asarray(shape),True))      
+         
+        if info['substrate'] == 'boulders':
+            r_patch.append(Polygon(np.asarray(shape),True))             
+    ax.add_collection(PatchCollection(s_patch, facecolor = colors[4],alpha=a_val, edgecolor='none',zorder=10))    
+    ax.add_collection(PatchCollection(g_patch, facecolor = colors[2],alpha=a_val, edgecolor='none',zorder=10))    
+    ax.add_collection(PatchCollection(r_patch, facecolor = colors[0],alpha=a_val, edgecolor='none',zorder=10)) 
+    ax.legend((circ5, circ3,circ1),('Sand','Gravel','Boulders'),numpoints=1, loc='best')
+
+    ax1.set_title('May 2014 Acousic \n Sediment Classifications')
+    m = Basemap(projection='merc', 
+                epsg=cs2cs_args.split(':')[1], 
+                llcrnrlon=np.nanmin(may_lon)-0.0001, 
+                llcrnrlat=np.nanmin(may_lat)-0.0002,
+                urcrnrlon=np.nanmax(may_lon)+0.0001, 
+                urcrnrlat=np.nanmax(may_lat)+0.0002,ax=ax1)
+    m.wmsimage(server='http://grandcanyon.usgs.gov/arcgis/services/Imagery/ColoradoRiverImageryExplorer/MapServer/WmsServer?', layers=['3'], xpixels=1000)
+    x,y = m.projtran(may_lon, may_lat)
+    im = m.contourf(x,y,may_sed_class.T, cmap='coolwarm', levels=[0,1,2,3,4,5])
+    
+    ax1.legend((circ5, circ3,circ1),('Sand','Gravel','Rock'),numpoints=1, loc='best')
+    plt.tight_layout()
+    plt.show()
+    plt.savefig(root + os.sep + 'mb_ss_ground_truth_area.png',dpi=600)
